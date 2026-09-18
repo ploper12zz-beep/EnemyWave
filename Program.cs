52907a57b574718462b76347efe824320320b0ee
ploper12zz-beep <ploper12zz@gmail.com>
2026-09-18 09:01:57 +0700

    Revisi displaying enemy wave 1 to 100

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿for (int i = 1; i <= 100; i++)
 {
-    Console.WriteLine($"\nWave {i} incoming!");
-    Console.WriteLine($"Wave {i} is Cleared!");
+    Console.WriteLine($"Wave {i} incoming!");
+    Console.WriteLine($"Wave {i} is Cleared!\n");
 }
